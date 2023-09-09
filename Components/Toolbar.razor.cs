ef5acd8a46981ca7168032ca295d599abac2bb1c
AmirCharkhand <amir.charkhand@outlook.com>
2023-09-09 17:44:44 +0330

    behavior for chainging toolbar's buttons Disabled property implemented

---
+++ Components/Toolbar.razor.cs
@@ -0,0 +1,31 @@
+﻿namespace RPGClient.Components;
+
+public partial class Toolbar
+{
+    private bool _addBtnDisabled = false;
+    private bool _editBtnDisabled = true;
+    private bool _removeBtnDisabled = true;
+
+    public void ChangeButtonsState(int tablesSelectedRowsCount)
+    {
+        switch (tablesSelectedRowsCount)
+        {
+            case 0:
+                _addBtnDisabled = false;
+                _editBtnDisabled = true;
+                _removeBtnDisabled = true;
+                break;
+            case 1:
+                _addBtnDisabled = false;
+                _editBtnDisabled = false;
+                _removeBtnDisabled = false;
+                break;
+            case > 1:
+                _addBtnDisabled = false;
+                _editBtnDisabled = true;
+                _removeBtnDisabled = false;
+                break;
+        }
+        StateHasChanged();
+    }
+}
